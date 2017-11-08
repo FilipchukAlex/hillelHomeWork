9a23db8ca5aa442d3147c22ee12f39358a06e918
filipchukalex <filipvermut@gmail.com>
2017-11-08 14:08:27 +0200

    I had put spaces after commas and used formatted string

--- HW1.py
+++ HW1.py
@@ -4,12 +4,12 @@ import math
 a = -9
 b = 31
 c = 4
-print("Our values: \na =",a,"\nb =",b,"\nc =",c)
+print("Our values: \na = %s,\nb = %s,\nc = %s", % a, b, c)
 # Let's find and print the results
 print("Expressions and results:")
-print("a+b*(c/2) =",a + b * (c / 2))
-print("(a**2+b**2) % 2 =",(a**2 + b**2) % 2)
-print("(a+b)/12*c%4+b =",(a + b) / 12 * c % 4 + b)
-print("(a-b*c)/(a+b)%c =",(a - b * c) / (a + b) % c)
-print("|a-b|/(a+b)**3-cos(c) =",math.fabs(a - b) / (a + b)**3 - math.cos(c))
-print("(ln(1+c)/(-b))**4+|a| =",(math.log1p(c) / (-b))**4 + math.fabs(a))
+print("a+b*(c/2) =", a + b * (c / 2))
+print("(a**2+b**2) % 2 =", (a**2 + b**2) % 2)
+print("(a+b)/12*c%4+b =", (a + b) / 12 * c % 4 + b)
+print("(a-b*c)/(a+b)%c =", (a - b * c) / (a + b) % c)
+print("|a-b|/(a+b)**3-cos(c) =", math.fabs(a - b) / (a + b)**3 - math.cos(c))
+print("(ln(1+c)/(-b))**4+|a| =", (math.log1p(c) / (-b))**4 + math.fabs(a))
