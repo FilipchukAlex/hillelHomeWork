2a51f846eb15804695714f81ad51213c14919995
FilipchukAlex <33446391+FilipchukAlex@users.noreply.github.com>
2017-11-10 11:37:54 +0200

    Delete Hw7.py

--- Hw7.py
+++
@@ -1,7 +0,0 @@
-# Conversion of the American date format into European
-american_date = "05.17.2016"
-print('The American date format:', american_date)
-temp_date = american_date.split('.')
-european_date = temp_date[1],temp_date[0],temp_date[2]
-european_date = '.'.join(european_date)
-print("The result of conversion,the European data format:", european_date)
