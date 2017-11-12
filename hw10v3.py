1ac3f6b679e07fc6492b0e2cf60776146ddb36ab
filipchukalex <filipvermut@gmail.com>
2017-11-12 17:26:44 +0200

    changed vars

--- hw10v3.py
+++ hw10v3.py
@@ -1,8 +1,8 @@
-string = 'Leo Tolstoy*1828-08-28*1910-11-20'
-string2 = string.split('*')
-birth_date = string2[1]
-death_date = string2[2]
+name_dates = 'Leo Tolstoy*1828-08-28*1910-11-20'
+name_dates2 = name_dates.split('*')
+birth_date = name_dates2[1]
+death_date = name_dates2[2]
 birth_year = int(birth_date.split('-')[0])
 death_year = int(death_date.split('-')[0])
 age = death_year - birth_year
-print(string2[0], ",", str(age))
+print(name_dates2[0], ",", str(age))
