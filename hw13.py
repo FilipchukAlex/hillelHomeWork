608ed03ce71e0de47affdde0b40bbac513b29bd7
filipchukalex <filipvermut@gmail.com>
2017-11-15 14:05:30 +0200

    hw13 fixed format string

--- hw13.py
+++ hw13.py
@@ -3,7 +3,7 @@ import math
 leg1 = 12
 leg2 = 35
 
-#print("Legs of the right-angled triangle are %d and %d", % leg1, leg2)
+print("Legs of the right-angled triangle are %d and %d" % (leg1, leg2))
 
 def square_and_perimeter_right_triangle(leg1, leg2):
     square =  (leg1 * leg2) / 2
