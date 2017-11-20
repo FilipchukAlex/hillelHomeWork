b58c4bdace4a3a7fb6d0277d98f9b652389fc84e
filipchukalex <filipvermut@gmail.com>
2017-11-20 20:14:58 +0200

    hw19 need to compare with some number from range, not with 0

--- hw19.py
+++ hw19.py
@@ -1,12 +1,12 @@
 import random
 
 num_limit = 100
-lower_bound = -150
-upper_bound = 280
+lower_bound = -237
+upper_bound = -4
 
 
 def find_max(num_limit, lower_bound, upper_bound):
-    max_number = 0
+    max_number = random.randint(lower_bound, upper_bound)
     for i in range(num_limit):
         number = random.randint(lower_bound, upper_bound)
         if number >= max_number:
