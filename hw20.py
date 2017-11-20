75443f6775e7a9b5dc4a704061e71421e45f51ed
filipchukalex <filipvermut@gmail.com>
2017-11-20 19:14:57 +0200

    hw20 fixed var name

--- hw20.py
+++ hw20.py
@@ -6,14 +6,14 @@ upper_bound = 280
 
 def diff_even_odd(num_limit, lower_bound, upper_bound):
     even_total_sum = 0
-    not_even_total_sum = 0
+    odd_total_sum = 0
     for i in range(num_limit):
         number = random.randint(lower_bound, upper_bound)
         if number % 2 == 0:
             even_total_sum += number
         else:
-            not_even_total_sum += number
-    return even_total_sum - not_even_total_sum
+            odd_total_sum += number
+    return even_total_sum - odd_total_sum
 
 
 print(diff_even_odd(num_limit, lower_bound, upper_bound))
