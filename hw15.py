58b098fe0347c83f53a411e9f8ee182670e6f084
filipchukalex <filipvermut@gmail.com>
2017-11-20 14:28:24 +0200

    hw15

--- hw15.py
+++ hw15.py
@@ -1,12 +1,12 @@
 import math
-x1 = 3; y1 = -2; r1 = 7
-x2 = -8; y2 = 4; r2 = 5
+x1 = 0; y1 = 7; r1 = 4
+x2 = 0; y2 = 0; r2 = 4
 
 
 def intersection_of_two_circles(x1, y1, r1, x2, y2, r2):
     centre_to_centre_distance = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
     d = centre_to_centre_distance
-    return d < r1 + r2 and d > abs(r1 - r2)
+    return (d < r1 + r2 and d > abs(r1 - r2)) or  (d == 0 and r1 == r2) or (d == r1 + r2) or (r1 == d + r2)
 
 
 if intersection_of_two_circles(x1, y1, r1, x2, y2, r2):
