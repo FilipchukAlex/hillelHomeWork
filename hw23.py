c39f71addaa49b1dd92e24dc114ddcda93df4b55
filipchukalex <filipvermut@gmail.com>
2017-11-22 21:20:59 +0200

    hw23

--- hw23.py
+++ hw23.py
@@ -1,12 +1,14 @@
 import random
+
 comp_number = random.randint(1,10)
-user_number = int(input("Enter the number 1...10: "))
+user_number = int(input("Enter the number from 1 to 10: "))
+
 while user_number != comp_number:
     if user_number > comp_number:
         print("My number is smaller")
-        user_number = int(input("Enter the number 1...10: "))
+        user_number = int(input("Enter the number: "))
     else:
         print("My number is bigger")
-        user_number = int(input("Enter the number: 1...10 "))
+        user_number = int(input("Enter the number:"))
 else:
     print("Yes, it is %d" % comp_number)
