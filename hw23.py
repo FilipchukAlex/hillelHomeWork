2cfaaaa48702a4b33908216c4b81402c2b909ac5
filipchukalex <filipvermut@gmail.com>
2017-11-22 21:54:06 +0200

    hw23 removed else

--- hw23.py
+++ hw23.py
@@ -10,6 +10,6 @@ while user_number != comp_number:
     else:
         print("My number is bigger")
         user_number = int(input("Enter the number:"))
-else:
-    print("Yes, it is %d" % comp_number)
-    print(input("Press Enter to quit"))
+
+print("Yes, it is %d" % comp_number)
+print(input("Press Enter to quit"))
