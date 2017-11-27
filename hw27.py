6d1255461ffa762a9afedbadbf5dca518bb126ff
filipchukalex <filipvermut@gmail.com>
2017-11-27 18:23:47 +0200

    hw27

--- hw27.py
+++ hw27.py
@@ -1,6 +1,12 @@
 def gen_primes():
-    result = [int(digit) for digit in range(1, 101)]
-    return result
+    lst_primes =[]
+    for i in range(2, 101):
+        for j in range(2, i):
+            if i % j == 0:
+                break
+        else:
+            lst_primes.append(i)
+    return lst_primes
 
 
 print(gen_primes())
