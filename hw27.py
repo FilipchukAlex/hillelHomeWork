1ddbbd93f01f4c7075627b94c33d09f7242c5cd2
filipchukalex <filipvermut@gmail.com>
2017-11-28 10:48:18 +0200

    hw27

--- hw27.py
+++ hw27.py
@@ -1,7 +1,11 @@
 def gen_primes():
+    import math
     lst_primes =[]
     for i in range(2, 101):
         for j in range(2, i):
+            if j > math.sqrt(i):
+                lst_primes.append(i)
+                break
             if i % j == 0:
                 break
         else:
