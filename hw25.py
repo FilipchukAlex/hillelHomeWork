e9087ca7ea03063ba4346c15fb02fdb5d5f36a33
FilipchukAlex <filipvermut@gmail.com>
2017-12-05 21:22:45 +0200

    hw25

--- hw25.py
+++ hw25.py
@@ -3,8 +3,11 @@ list_to_shuffle = [digit for digit in range(1, 100, 2)]
 
 def shuffle_list(list_to_shuffle):
     import random
-    while list_to_shuffle:
-        print(list_to_shuffle.pop(random.randint(0, len(list_to_shuffle) - 1)))
+    for i in range(len(list_to_shuffle)):
+        a = random.randint(0, len(list_to_shuffle) - 1)
+        b = random.randint(0, len(list_to_shuffle) - 1)
+        list_to_shuffle[a], list_to_shuffle[b] = list_to_shuffle[b], list_to_shuffle[a]
+    return list_to_shuffle
 
 
 print(shuffle_list(list_to_shuffle))
