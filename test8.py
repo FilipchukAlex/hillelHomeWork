ec41431d8517ad3893caa2f85a400a4b4e05b70b
filipchukalex <filipvermut@gmail.com>
2017-12-06 15:42:34 +0200

    test8

--- test8.py
+++ test8.py
@@ -1,12 +1,9 @@
-lst = [2, 5, 78, 45,1]
+lst = [2, 5, 78, 45, 1]
 
 
 def replace_max_min(lst):
-    if lst.index(min(lst)) < lst.index(max(lst)):
-        lst[lst.index(max(lst))], lst[lst.index(min(lst))]  =  min(lst), max(lst)
-    else:
-        lst[lst.index(min(lst))], lst[lst.index(max(lst))] = max(lst), min(lst)
+    idx_min, idx_max = lst.index(min(lst)), lst.index(max(lst))
+    lst[idx_min], lst[idx_max] = lst[idx_max], lst[idx_min]
     return lst
-
 
 print(replace_max_min(lst))
