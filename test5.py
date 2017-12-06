a1ca4916f9b7f8f61264a059d4b76f76c7cf0586
filipchukalex <filipvermut@gmail.com>
2017-12-06 16:20:33 +0200

    test5

---
+++ test5.py
@@ -0,0 +1,7 @@
+number1 = float(input("Please, enter the first number: "))
+number2 = float(input("Please, enter the second number: "))
+
+if abs(10 - number1) < abs(10 - number2):
+    print(number1)
+else:
+    print(number2)
