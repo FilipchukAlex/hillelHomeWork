e6fd7f2334e3920c96fa493b0a61a561a901eb2f
FilipchukAlex <filipvermut@gmail.com>
2017-12-12 20:02:26 +0200

    test11 with transposed matrix by hands

--- test11.py
+++ test11.py
@@ -2,13 +2,22 @@ matrix = [[2,3,5,2],[2,4,6,2],[-2,7,2,0]]
 #matrix = [[5, 6, 4], [-2, 5, 3], [8, 7, -2]]
 #matrix = [[3,2,1],[1,3,4]]
 
-new_matrix = [list(i) for i in zip(*matrix)]
 
-for i  in range(len(new_matrix)):
-    if i in range(0, len(new_matrix), 2):
-        new_matrix[i].sort(reverse=True)
+def matrix_transpose(matrix):
+    new_matrix = [[0  for i in range(len(matrix))] for j in range(len(matrix[0]))]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            new_matrix[j][i] = matrix[i][j]
+    return new_matrix
+
+
+matrix_transposed = matrix_transpose(matrix)
+
+for i  in range(len(matrix_transposed)):
+    if i in range(0, len(matrix_transposed), 2):
+        matrix_transposed[i].sort(reverse=True)
     else:
-        new_matrix[i].sort()
+        matrix_transposed[i].sort()
 
-matrix = [list(i) for i in zip(*new_matrix)]
 print(matrix)
+print(matrix_transpose(matrix_transposed))
